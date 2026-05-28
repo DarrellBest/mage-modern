5d7bce01c53a534c7d6c4ff13c7d2068ffee5a07
Darrell Best <darrellbest@gmail.com>
2026-05-28 14:36:06 -0400

    lobby: tighten right pane closer to 25% (push divider to 0.78)

--- Mage.Client/src/main/java/mage/client/table/TablesPanel.java
+++ Mage.Client/src/main/java/mage/client/table/TablesPanel.java
@@ -639,9 +639,9 @@ public class TablesPanel extends javax.swing.JPanel {
         tableCompleted.setRowHeight(GUISizeHelper.tableRowHeight);
 
         jSplitPane1.setDividerSize(GUISizeHelper.dividerBarSize);
-        // right pane (players + chat) ~ 25% of the lobby width; resize-weight 0.75 keeps the ratio on window resize
-        jSplitPane1.setResizeWeight(0.75);
-        javax.swing.SwingUtilities.invokeLater(() -> jSplitPane1.setDividerLocation(0.75));
+        // right pane (players + chat) ~ 25% of the lobby width; the chat has a minimum so push past 0.75
+        jSplitPane1.setResizeWeight(0.78);
+        javax.swing.SwingUtilities.invokeLater(() -> jSplitPane1.setDividerLocation(0.78));
         jSplitPaneTables.setDividerSize(GUISizeHelper.dividerBarSize);
         jScrollPaneTablesActive.getVerticalScrollBar().setPreferredSize(new Dimension(GUISizeHelper.scrollBarSize, 0));
         jScrollPaneTablesActive.getHorizontalScrollBar().setPreferredSize(new Dimension(0, GUISizeHelper.scrollBarSize));
